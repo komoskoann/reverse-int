adf2dd37a7c48ae897ce3b2a0cd0f06bb9e15f11
komoskoann\\----------- <komoskoann@gmail.com>
2021-08-09 16:30:33 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString().split('').reverse().join('');
+    if (n > 0) {
+        return +str;
+    } else {
+        return +str.slice(0,str.length - 1);
+    }
 }
